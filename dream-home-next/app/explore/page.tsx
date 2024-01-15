53bd8cd2ffa93e94ff1ef5db7f5af06f2fd4dba7
Clare Dang <claredng@gmail.com>
2024-01-14 23:23:22 -0800

    explore card styling

---
+++ dream-home-next/app/explore/page.tsx
@@ -0,0 +1,20 @@
+import { fetchExploreImage } from "../action";
+
+import LoadMore from "../../components/LoadMore";
+
+export default async function Explore() {
+  const data = await fetchExploreImage(1);
+  return (
+    <>
+      <main className="sm:p-16 px-8 flex flex-col gap-10 items-center">
+        <h2 className="text-3xl text-black font-bold">
+          Explore All Interior Design
+        </h2>
+        <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6">
+          {data}
+        </section>
+        <LoadMore />
+      </main>
+    </>
+  );
+}
