02c932ad179890e64218a3b05f62ee2544ffef8f
Clare Dang <claredng@gmail.com>
2024-01-18 16:53:48 -0800

    ignore eslint  to test

--- dream-home-next/next.config.js
+++ dream-home-next/next.config.js
@@ -12,6 +12,18 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
