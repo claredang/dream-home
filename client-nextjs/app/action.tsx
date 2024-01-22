a9d60b4f320b30fcc9044a1e948f41a6d0d8d736
Clare Dang <claredng@gmail.com>
2024-01-21 22:31:46 -0800

    call chatbot API from server

--- client-nextjs/app/action.tsx
+++ client-nextjs/app/action.tsx
@@ -4,10 +4,17 @@ import ExploreCard, { ExploreProp } from "@/app/explore/ExploreCard";
 const MAX_LIMIT = 8;
 
 export async function fetchExploreImage(page: number) {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER}/explore`);
+  // const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER}/explore`);
+  const response = await fetch("http://localhost:8080/explore");
   const data = await response.json();
 
   return data.map((explore: ExploreProp, index: number) => (
     <ExploreCard key={explore.id} explore={explore} index={index} />
   ));
+}
+
+export async function fetchChatbotKey() {
+  const response = await fetch(`http://localhost:8080/chatbot`);
+  const data = await response.json();
+  return data.chatbot;
 }
