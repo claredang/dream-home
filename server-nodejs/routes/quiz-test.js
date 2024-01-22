054d7403999f977bb4086150497b574aa5c8d04a
Clare Dang <claredng@gmail.com>
2024-01-21 18:10:30 -0800

    seperate quiz-test route

--- server-nodejs/routes/quiz-test.js
+++ server-nodejs/routes/quiz-test.js
@@ -1,50 +1,126 @@
 const express = require("express");
 
-// The router is a middleware
-const recordRoutes = express.Router();
+const quizRoutes = express.Router();
 
-// Connect to the database
 const dbo = require("../db/quiz-test");
 
+const sessions = {};
+let questionsData = "";
+
+// Call the async function
+async function getQuizQuestion() {
+  const cursor = dbo.getQuizTestDb();
+  questionsData = await cursor;
+}
+getQuizQuestion();
+
 // GET: ALL
-recordRoutes.route("/quiz-test").get(async function (req, res) {
+quizRoutes.route("/quiz-test").get(async function (req, res) {
   const cursor = dbo.getQuizTestDb();
   var results = await cursor;
   res.json(results).status(200);
 });
 
-// GET: Query
-recordRoutes.route("/api/query").post(async function (req, res) {
-  const { location } = req.body;
-  console.log("inside here", location);
-  const cursor = dbo.queryDb(location);
-  var results = await cursor;
-  res.json(results).status(200);
+// POST: add quiz test
+quizRoutes.route("/quiz/start").post((req, res) => {
+  const sessionId = generateSessionId();
+  const firstQuestion = getNextQuestion(sessionId);
+  res.json({ sessionId, question: firstQuestion });
 });
 
-// POST: Create one
-recordRoutes.route("/explore/new").post(async function (req, res) {
-  const result = dbo.queryDb(req);
-  if (result) res.send("Not found").status(404);
-  res.send(result).status(204);
+// POST: user answer
+quizRoutes.route("/quiz/answer").post((req, res) => {
+  const { sessionId, answer } = req.body;
+  storeUserAnswer(sessionId, answer);
+
+  const nextQuestion = getNextQuestion(sessionId);
+  if (nextQuestion) {
+    res.json({ question: nextQuestion });
+  } else {
+    const result = calculateResult(sessionId);
+    res.json({ result });
+  }
 });
 
-// POST: Add col
-recordRoutes.route("/explore/add-col").post((req, res) => {
-  const _db = dbo.addCol();
-  res.send(_db).status(200);
+quizRoutes.route("/quiz/result/:sessionId").post((req, res) => {
+  const sessionId = req.params.sessionId;
+  const result = calculateResult(sessionId);
+  res.json({ result });
 });
 
-// POST: Del col
-recordRoutes.route("/explore/del-col").post((req, res) => {
-  const _db = dbo.delCol();
-  res.send(_db).status(200);
-});
+// HELPER FUNCTION:
+function generateSessionId() {
+  return Math.random().toString(36).substring(2, 15);
+}
 
-// DELETE: Delete ALL
-recordRoutes.route("/explore/delete").delete((req, res) => {
-  const _db = dbo.deleteDb();
-  res.send(_db).status(200);
-});
+function getNextQuestion(sessionId) {
+  const session = sessions[sessionId] || initializeSession(sessionId);
+  const { currentQuestionIndex } = session;
 
-module.exports = recordRoutes;
+  if (currentQuestionIndex < questionsData.length) {
+    const nextQuestion = questionsData[currentQuestionIndex];
+    return {
+      id: nextQuestion.id,
+      question: nextQuestion.question,
+      options: nextQuestion.options.map((option) => ({
+        text: option.text,
+        image: option.image,
+        type: option.type,
+      })),
+    };
+  }
+
+  return null;
+}
+function storeUserAnswer(sessionId, answer) {
+  const session = sessions[sessionId] || initializeSession(sessionId);
+  const { currentQuestionIndex, userAnswers } = session;
+  const currentQuestion = questionsData[currentQuestionIndex];
+
+  userAnswers[currentQuestion._id] = answer;
+
+  session.currentQuestionIndex += 1;
+}
+
+function calculateResult(sessionId) {
+  const session = sessions[sessionId];
+  const { userAnswers } = session;
+  const styleCount = {};
+
+  for (const questionId in userAnswers) {
+    const answerType = userAnswers[questionId];
+    if (answerType in styleCount) {
+      styleCount[answerType] += 1;
+    } else {
+      styleCount[answerType] = 1;
+    }
+  }
+
+  const maxCount = Math.max(...Object.values(styleCount));
+
+  const mostPreferredStyles = Object.keys(styleCount).filter(
+    (style) => styleCount[style] === maxCount
+  );
+
+  const styleDescriptions = dbo.getStyleDescription();
+  const resultDescriptions = mostPreferredStyles.map(
+    (style) => styleDescriptions[style]
+  );
+
+  const result = mostPreferredStyles.map((style, index) => ({
+    [style]: resultDescriptions[index],
+  }));
+  console.log("Combined Result:", result);
+  return result;
+}
+
+function initializeSession(sessionId) {
+  const session = {
+    currentQuestionIndex: 0,
+    userAnswers: {},
+  };
+  sessions[sessionId] = session;
+  return session;
+}
+
+module.exports = quizRoutes;
--- server-nodejs/server.js
+++ server-nodejs/server.js
@@ -15,6 +15,8 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
+app.use(require("./routes/quiz-test"));
+
 // get driver connection
 const dbo = require("./db/conn");
 
