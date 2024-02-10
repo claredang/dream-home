d43982e54e924a24679cef5500ea7d9cad6578f7
Clare Dang <claredng@gmail.com>
2024-02-10 10:57:27 -0800

    footer styling

--- client-nextjs/app/_components/Footer.tsx
+++ client-nextjs/app/_components/Footer.tsx
@@ -7,11 +7,7 @@ const Footer = () => {
     <footer className="flexCenter mb-24">
       <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
-          {/* <Link href="/" className="mb-10">
-            <Image src="hilink-logo.svg" alt="logo" width={74} height={29} />
-          </Link> */}
-
-          <Link href="/">
+          <Link href="/" className="mb-5">
             <Image
               src="./logo-nav-no-bg.svg"
               alt="spinner"
@@ -20,7 +16,6 @@ const Footer = () => {
               className="object-contain"
             />
           </Link>
-
           <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
             {FOOTER_LINKS.map((columns, index) => (
               <FooterColumn title={columns.title} key={index}>
