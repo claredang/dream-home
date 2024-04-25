f3e26e5087b240fa0e0a8329e55d165b1ebd01bc
Clare Dang <claredng@gmail.com>
2024-04-25 10:53:52 +0700

    rewrite appspec.yml

--- server-nodejs/scripts/application_start.sh
+++ server-nodejs/scripts/application_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'run application_start.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+echo 'run application_start.sh: ' >> /home/ubuntu/dream-home/server-nodejs/deploy.log
 
-echo 'pm2 restart dream-home-server-ssl' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
-pm2 restart dream-home-server-ssl >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+echo 'pm2 restart dream-home-server-ssl' >> /home/ubuntu/dream-home/server-nodejs/deploy.log
+pm2 restart dream-home-server-ssl >> /home/ubuntu/dream-home/server-nodejs/deploy.log
