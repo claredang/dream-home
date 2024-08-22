7c202938e87107a8fd00accd9bfddc274dd43332
Clare Dang <claredng@gmail.com>
2024-08-21 17:58:48 -0700

    style change for Image Generator page

--- client-nextjs/app/(default-layout)/image-generator/page.tsx
+++ client-nextjs/app/(default-layout)/image-generator/page.tsx
@@ -10,7 +10,7 @@ export default function ImageGenerator() {
   const [styles, setStyles] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const roomTypes = ["Living Room", "Bedroom", "Kitchen"];
+  const roomTypes = ["Living Room", "Dining Room", "Bedroom", "Office"];
   const styleOptions = [
     "Bohemian",
     "Coastal",
@@ -18,7 +18,7 @@ export default function ImageGenerator() {
     "Glam",
     "Industrial",
     "Mid-century Modern",
-    "Minimalist",
+    "Indochine",
     "Scandinavian",
   ];
 
@@ -35,41 +35,44 @@ export default function ImageGenerator() {
     setLoading(true);
     const stylesString = styles.join(", ").toLowerCase();
     const fullPrompt = `Generate the ${roomType.toLowerCase()} in ${stylesString} style(s) that is ${customize}`;
-    console.log("full customize: ", fullPrompt);
 
     try {
-      const response = await fetch(`http://localhost:8080/api/generate-image`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          fullPrompt,
-        }),
-      });
+      //   const response = await fetch(`http://localhost:8080/api/generate-image`, {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER}/api/generate-image`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            fullPrompt,
+          }),
+        }
+      );
       const data = await response.json();
       setImage(data.image);
     } catch (error) {
       console.error("Error fetching image:", error);
     } finally {
-      setLoading(false); // Stop loading after response
+      setLoading(false);
     }
   };
 
   return (
-    <div class="gap-1 px-5 flex flex-col md:flex-row flex-1 justify-center py-5">
-      <div class="layout-content-container flex flex-col w-96">
-        <h1 class="text-[#1b140e] tracking-light text-[20px] font-bold leading-tight px-4 pb-3 pt-6">
+    <div className="gap-1 px-5 flex flex-col md:flex-row flex-1 justify-center py-5">
+      <div className="layout-content-container flex flex-col w-96">
+        <h1 className="text-[#1b140e] tracking-light text-[20px] font-bold leading-tight px-4 pb-3 pt-6">
           What type of room are you designing?
         </h1>
-        <div class="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
-          <label class="flex flex-col min-w-40 flex-1">
-            <p class="text-[#1b140e] text-base font-medium leading-normal pb-2">
+        <div className="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
+          <label className="flex flex-col min-w-40 flex-1">
+            <p className="text-[#1b140e] text-base font-medium leading-normal pb-2">
               Room Type
             </p>
             <select
-              class="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#1b140e] focus:outline-0 focus:ring-0 border-none bg-[#f3ede7] focus:border-none h-14 bg-[image:--select-button-svg] placeholder:text-[#97704e] p-4 text-base font-normal leading-normal"
+              className="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#1b140e] focus:outline-0 focus:ring-0 border-none bg-[#f3ede7] focus:border-none h-14 bg-[image:--select-button-svg] placeholder:text-[#97704e] p-4 text-base font-normal leading-normal"
               value={roomType}
               onChange={(e) => setRoomType(e.target.value)}
-              //   required
+              required
             >
               <option value="" disabled>
                 Select a room type
@@ -82,64 +85,61 @@ export default function ImageGenerator() {
             </select>
           </label>
         </div>
-        <h1 class="text-[#1b140e] tracking-light text-[20px] font-bold leading-tight px-4 pb-3 pt-6">
+        <h1 className="text-[#1b140e] tracking-light text-[20px] font-bold leading-tight px-4 pb-3 pt-6">
           Which style do you prefer?
         </h1>
-        <div class="flex gap-3 p-3 flex-wrap pr-4">
+        <div className="flex gap-3 p-3 flex-wrap pr-4">
           {styleOptions.map((styleOption) => (
             <div
               key={styleOption}
-              class={`flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl pl-4 pr-4 cursor-pointer ${
+              className={`flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl pl-4 pr-4 cursor-pointer ${
                 styles.includes(styleOption)
                   ? "bg-[#e88630] text-white"
                   : "bg-[#f3ede7] text-[#1b140e]"
               }`}
               onClick={() => handleStyleToggle(styleOption)}
             >
-              <p class="text-sm font-medium leading-normal">{styleOption}</p>
+              <p className="text-sm font-medium leading-normal">
+                {styleOption}
+              </p>
             </div>
           ))}
           <button className="btn-yellow">
-            <Link
-              href="/quiz-test"
-              target="_blank"
-              //   class="bg-yellow-100 flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl pl-3 pr-3 cursor-pointer"
-            >
+            <Link href="/quiz-test" target="_blank">
               Quiz Your Style
             </Link>
           </button>
         </div>
         <form onSubmit={handleSubmit}>
-          <div class="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
-            <label class="flex flex-col min-w-40 flex-1">
-              <p class="text-[#1b140e] text-base font-medium leading-normal pb-2">
+          <div className="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
+            <label className="flex flex-col min-w-40 flex-1">
+              <p className="text-[#1b140e] text-base font-medium leading-normal pb-2">
                 Custom Preferences
               </p>
               <textarea
                 placeholder="Facing the beach.."
                 value={customize}
                 onChange={(e) => setCustomize(e.target.value)}
-                d
-                class="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#1b140e] focus:outline-0 focus:ring-0 border-none bg-[#f3ede7] focus:border-none min-h-36 placeholder:text-[#97704e] p-4 text-base font-normal leading-normal"
+                // required
+                className="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#1b140e] focus:outline-0 focus:ring-0 border-none bg-[#f3ede7] focus:border-none min-h-36 placeholder:text-[#97704e] p-4 text-base font-normal leading-normal"
               ></textarea>
             </label>
           </div>
 
-          <div class="flex px-4 py-3">
+          <div className="flex px-4 py-3">
             <button
               type="submit"
-              class="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 flex-1 bg-[#e88630] text-[#1b140e] text-sm font-bold leading-normal tracking-[0.015em]"
+              className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 flex-1 bg-[#e88630] text-[#1b140e] text-sm font-bold leading-normal tracking-[0.015em]"
             >
-              <span class="truncate">Generate Image</span>
+              <span className="truncate">Generate Image</span>
             </button>
           </div>
         </form>
       </div>
-      <div class="layout-content-container flex flex-col max-w-[700px] flex-1">
-        <div class="flex px-4 py-3">
+      <div className="layout-content-container flex flex-col max-w-[700px] flex-1">
+        <div className="flex px-4 py-3">
           {loading && (
             <div className="min-h-screen flex justify-center items-center">
-              <p>Calculating your result</p>
               <div className="relative">
                 <Image
                   src="/spinner-square.svg"
