4fa3fe1699b78183105b5497cfb647f6fb65b330
Clare Dang <claredng@gmail.com>
2024-08-21 17:49:30 -0700

    add spinner loading for image generator

--- client-nextjs/app/(default-layout)/image-generator/page.tsx
+++ client-nextjs/app/(default-layout)/image-generator/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function ImageGenerator() {
-  const [prompt, setPrompt] = useState("");
+  const [customize, setCustomize] = useState("");
   const [image, setImage] = useState("");
   const [roomType, setRoomType] = useState("");
   const [styles, setStyles] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const roomTypes = ["Living Room", "Bedroom", "Kitchen"];
   const styleOptions = [
@@ -29,27 +32,31 @@ export default function ImageGenerator() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     const stylesString = styles.join(", ").toLowerCase();
-    const fullPrompt = `Generate the ${roomType.toLowerCase()} in ${stylesString} style(s) that is ${prompt}`;
-    console.log("full prompt: ", fullPrompt);
-    const response = await fetch(
-      //   `${process.env.NEXT_PUBLIC_SERVER}/design-inspiration-user`,
-      `http://localhost:8080/api/generate-image`,
-      {
+    const fullPrompt = `Generate the ${roomType.toLowerCase()} in ${stylesString} style(s) that is ${customize}`;
+    console.log("full customize: ", fullPrompt);
+
+    try {
+      const response = await fetch(`http://localhost:8080/api/generate-image`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           fullPrompt,
         }),
-      }
-    );
-    const data = await response.json();
-    setImage(data.image);
+      });
+      const data = await response.json();
+      setImage(data.image);
+    } catch (error) {
+      console.error("Error fetching image:", error);
+    } finally {
+      setLoading(false); // Stop loading after response
+    }
   };
 
   return (
-    <div class="gap-1 px-6 flex flex-col md:flex-row flex-1 justify-center py-5">
-      <div class="layout-content-container flex flex-col w-80">
+    <div class="gap-1 px-5 flex flex-col md:flex-row flex-1 justify-center py-5">
+      <div class="layout-content-container flex flex-col w-96">
         <h1 class="text-[#1b140e] tracking-light text-[20px] font-bold leading-tight px-4 pb-3 pt-6">
           What type of room are you designing?
         </h1>
@@ -62,7 +69,7 @@ export default function ImageGenerator() {
               class="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#1b140e] focus:outline-0 focus:ring-0 border-none bg-[#f3ede7] focus:border-none h-14 bg-[image:--select-button-svg] placeholder:text-[#97704e] p-4 text-base font-normal leading-normal"
               value={roomType}
               onChange={(e) => setRoomType(e.target.value)}
-              required
+              //   required
             >
               <option value="" disabled>
                 Select a room type
@@ -92,6 +99,15 @@ export default function ImageGenerator() {
               <p class="text-sm font-medium leading-normal">{styleOption}</p>
             </div>
           ))}
+          <button className="btn-yellow">
+            <Link
+              href="/quiz-test"
+              target="_blank"
+              //   class="bg-yellow-100 flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl pl-3 pr-3 cursor-pointer"
+            >
+              Quiz Your Style
+            </Link>
+          </button>
         </div>
         <form onSubmit={handleSubmit}>
           <div class="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
@@ -101,9 +117,9 @@ export default function ImageGenerator() {
               </p>
               <textarea
                 placeholder="Facing the beach.."
-                value={prompt}
-                onChange={(e) => setPrompt(e.target.value)}
-                required
+                value={customize}
+                onChange={(e) => setCustomize(e.target.value)}
+                d
                 class="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#1b140e] focus:outline-0 focus:ring-0 border-none bg-[#f3ede7] focus:border-none min-h-36 placeholder:text-[#97704e] p-4 text-base font-normal leading-normal"
               ></textarea>
             </label>
@@ -121,16 +137,26 @@ export default function ImageGenerator() {
       </div>
       <div class="layout-content-container flex flex-col max-w-[700px] flex-1">
         <div class="flex px-4 py-3">
-          {/* <button class="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 flex-1 bg-[#e88630] text-[#1b140e] text-sm font-bold leading-normal tracking-[0.015em]">
-            <span class="truncate">Upload Image</span>
-          </button> */}
+          {loading && (
+            <div className="min-h-screen flex justify-center items-center">
+              <p>Calculating your result</p>
+              <div className="relative">
+                <Image
+                  src="/spinner-square.svg"
+                  alt="Home Cover"
+                  width={200}
+                  height={200}
+                  className="h-full w-full"
+                />
+              </div>
+            </div>
+          )}
+          {!loading && image && (
+            <div style={{ marginTop: "20px" }}>
+              <img src={`data:image/png;base64,${image}`} alt="Generated" />
+            </div>
+          )}
         </div>
-        {image && (
-          <div style={{ marginTop: "20px" }}>
-            {/* <h2>Generated Image:</h2> */}
-            <img src={`data:image/png;base64,${image}`} alt="Generated" />
-          </div>
-        )}
       </div>
     </div>
   );
